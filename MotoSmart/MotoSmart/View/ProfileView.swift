7425b27fef2aed54318ac022df400bd95939c29b
metelykx <ivaschenkoodenys@gmail.com>
2025-01-07 00:27:19 +0300

    add custom tabBar

--- MotoSmart/MotoSmart/View/ProfileView.swift
+++ MotoSmart/MotoSmart/View/ProfileView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct ProfileView: View {
+    init() {
+            // Создаем объект appearance для настройки TabBar
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground() // Отключаем прозрачность
+            appearance.backgroundColor = UIColor(named: "back") // Устанавливаем цвет фона TabBar
+
+            // Применяем appearance для TabBar
+            UITabBar.appearance().standardAppearance = appearance
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
     var body: some View {
         
                 
@@ -23,7 +33,7 @@ struct ProfileView: View {
                         .tabItem { Image(systemName: "message.fill") }
                     FavouritesView()
                         .tabItem { Image(systemName: "heart.fill") }
-                }
+                }.accentColor(Color("Cars", bundle: nil)) // Цвет выбранной вкладки
             }
         }
     
